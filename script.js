c0cefee56f272ef9c7bdd6500100994d673f14ef
Kylee Bijerring <kylee24b@gmail.com>
2021-05-15 16:28:50 -0600

    code quiz

---
+++ script.js
@@ -0,0 +1,11 @@
+var playEl = document.querySelector(".play"); 
+var scoreEl = document.querySelector(".score");
+
+
+playEl.addEventListener("click" , function() {
+    alert("button clicked");
+});
+
+scoreEl.addEventListener("click", function () {
+    alert("button clicked");
+});
