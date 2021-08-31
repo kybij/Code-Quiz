e2a2a31f1cb838d4031ca6c573334d97e6ac32d2
Kylee Bijerring <kylee24b@gmail.com>
2021-08-30 21:15:52 -0600

    new changes to game

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ var availableQuestions = []
 var score_points = 100
 var max_questions = 5
 
+var timerId 
+
 
 var questions = [
     {
@@ -52,26 +54,89 @@ var questions = [
     }
 ];
 
+var timeLeft = questions.length * 15
+questions = questions.sort(question => 0.5 - Math.random() )
+
+console.log(questions)
+
 
 playGame = () => {
-    //countdown()
+
+
+  timerId =  setInterval(countDown, 1000)
+     
+    
     questionCounter = 0
     score = 0
-    availableQuestions = [...questions]
     getQuestions()
 
 };
 
-getQuestions = () => {
-    questionCounter++
-    var questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestions = availableQuestions[questionsIndex]
-    question.innerText = currentQuestions.question
+countDown = () => {
+    document.querySelector("#score").textContent = timeLeft-- 
+    if ( timeLeft < 0){
+        clearInterval(timerId)
+    }
+}
 
-    choices.forEach(choice => {
-        var number = choice.dataset["number"]
-        choice.innerText = currentQuestions["choice" + number]
-    })
+getQuestions = () => {
+    
+   
+  
+
+    document.querySelector(".questionContainer").innerHTML = `
+    <h1 id="question">${questions[questionCounter].question}</h1>
+    <div class="choice-container">
+       <button class="btn" data-id=1 >${questions[questionCounter].choice1}</button>
+        
+    </div>
+    <div class="choice-container">
+    <button class="btn"  data-id=2 >${questions[questionCounter].choice2}</button>
+         
+    </div>
+    <div class="choice-container">
+        <button class="btn"  data-id=3 >${questions[questionCounter].choice3}</button>
+        
+    </div>
+    </div>
+    `
+
+
+     var btnArraySelector = document.querySelectorAll(".btn")
+
+    for (let index = 0; index < btnArraySelector.length; index++) {
+        btnArraySelector[index].addEventListener("click", function () {
+              var answer = this.getAttribute("data-id")
+             if (questionCounter < questions.length ) {
+                    
+                if (answer !=questions[questionCounter].a){
+                    timeLeft = timeLeft -15
+                    document.querySelector(".errorMessage").textContent= "Wrong"
+                }
+                else {
+                    document.querySelector(".errorMessage").textContent= "Right"
+                }
+
+                questionCounter++
+            
+                setTimeout(getQuestions, 2000)
+             }
+             else {
+                 clearInterval(timerId)
+             }
+           
+           
+        })
+    }
+
+
+    // currentQuestions = availableQuestions[questionsIndex]
+    // question.innerText = currentQuestions.question
+
+    // choices.forEach(choice => {
+    //     var number = choice.dataset["number"]
+    //     choice.innerText = currentQuestions["choice" + number]
+    // })
 };
 
 
